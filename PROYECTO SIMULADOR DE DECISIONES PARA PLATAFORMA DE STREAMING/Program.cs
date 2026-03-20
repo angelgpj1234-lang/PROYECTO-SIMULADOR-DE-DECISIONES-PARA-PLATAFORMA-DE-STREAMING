1bf0b44364d95e7370eb311bba481401b2c9f712
Angelowws <angel.gpj1234@gmail.com>
2026-03-19 21:04:17 -0600

    segunda fase de proyecto

--- PROYECTO SIMULADOR DE DECISIONES PARA PLATAFORMA DE STREAMING/Program.cs
+++ PROYECTO SIMULADOR DE DECISIONES PARA PLATAFORMA DE STREAMING/Program.cs
@@ -1,43 +1,138 @@
-﻿Console.WriteLine("====MENU===");
-Console.WriteLine("1. Tipo de contenido (película, serie, documental, evento en vivo");
-Console.WriteLine("2. Duración en minutos");
-Console.WriteLine("3. Clasificación (todo público, +13, +18)");
-Console.WriteLine("4. Hora programada (0–23)");
-Console.WriteLine("5. Nivel de producción (bajo, medio, alto)");
-Console.WriteLine("0. Salir");
+﻿using System.ComponentModel.Design;
+using System.Reflection.Metadata;
+int opcion;
 
-int opcion = int.Parse(Console.ReadLine());
-switch (opcion)
+
+do
 {
-    case 1:
+    int opcioncontenido;
+    int pelicula;
+    int serie;
+    int docuemntal;
+    int eventoenvivo;
+    Console.Clear();
+    Console.WriteLine("====MENU===");
+    Console.WriteLine("1. Evaluación de contenido");
+    Console.WriteLine("2. Mostrar las reglas del sistema");
+    Console.WriteLine("3. Estadisticas de sesión");
+    Console.WriteLine("4. Reiniciar estadisticas");
+    Console.WriteLine("5. Salir");
+    opcion = int.Parse(Console.ReadLine());
 
-    break;
-    case 2:
-        Console.WriteLine("Duracion en minutos");
-        int duracion = int.Parse(Console.ReadLine());
-        if (duracion >= 120)
-        {
-            Console.WriteLine("impacto alto");
-        }
-        else if (duracion >= 60 && duracion <= 120)
-        {
-            Console.WriteLine("impacto medio");
-        }
-        else
-        {
-            Console.WriteLine("impacto bajo");
-        }
+    switch (opcion)
+    {
+
+        case 1:
+            do { 
+                Console.Clear() ;
+                Console.WriteLine("Ingrese la duración del contenido");
+                Console.WriteLine("Seleccione su tipo de contenido");
+                Console.WriteLine("1. Pelicula");
+                Console.WriteLine("2. Serie");
+                Console.WriteLine("3. Documental");
+                Console.WriteLine("4. Evento en vivo");
+                Console.WriteLine("0. Salir y regresar al menu de contenido");
+                opcioncontenido = int.Parse(Console.ReadLine());
+                switch (opcioncontenido)
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine("Ingrese la duracion en minutos de la Pelicula");
+                        pelicula = int.Parse(Console.ReadLine());
+                        if (pelicula <= 60 || pelicula >= 180)
+                        {
+                            Console.WriteLine("el contenido no cumple la validación");
+                        }
+                        else
+                        {
+                            Console.WriteLine("El contenido se registro");
+                        }
+                        regresaralmenu();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        Console.WriteLine("Ingrese la duracion en minutois de la Serie");
+                        serie = int.Parse(Console.ReadLine());
+                        if (serie <= 20 || serie >= 90)
+                        {
+                            Console.WriteLine("el contenido no cumple la validación");
+                        }
+                        else
+                        {
+                            Console.WriteLine("El contenido se registro");
+                        }
+                        regresaralmenu();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        Console.WriteLine("Ingrese la duracion en minutois del Documental");
+                        docuemntal = int.Parse(Console.ReadLine());
+                        if (docuemntal <= 30 || docuemntal >= 120)
+                        {
+                            Console.WriteLine("el contenido no cumple la validación");
+                        }
+                        else
+                        {
+                            Console.WriteLine("El contenido se registro");
+                        }
+                        regresaralmenu();
+                        break;
+                    case 4:
+                        Console.Clear();
+                        Console.WriteLine("Ingrese la duracion en minutois del Evento en vivo");
+                        eventoenvivo = int.Parse(Console.ReadLine());
+                        if (eventoenvivo <= 30 || eventoenvivo >= 240)
+                        {
+                            Console.WriteLine("el contenido no cumple la validación");
+                        }
+                        else
+                        {
+                            Console.WriteLine("El contenido se registro");
+                        }
+                        regresaralmenu();
+                        break;
+                    case 0:
+                        Console.WriteLine("Saliendo....");
+                        break;
+                    default:
+                        Console.WriteLine("Opcion invalida");
+                        break;
+                }
+            } while (opcioncontenido != 0) ;
+            regresaralmenu();
             break;
-    case 3:
+        case 2:
+            {
+                Console.WriteLine("Reglas del sistema");
+                Console.WriteLine("Reglas de clasificación y horario\r\n• Todo público: cualquier hora\r\n• +13: entre 6 y 22 horas\r\n• +18: entre 22 y 5 horas\r\nReglas de duración por tipo\r\n• Película: 60–180 minutos\r\n• Serie: 20–90 minutos\r\n• Documental: 30–120 minutos\r\n• Evento en vivo: 30–240 minutos\r\nSi la duración está fuera del rango permitido, el contenido no cumple la validación técnica.\r\nReglas de producción\r\n• Producción baja solo válida para Todo público o +13\r\n• Producción media o alta válida para cualquier clasificación");
+            }
+        break;
+        case 3:
+            {
+                Console.WriteLine("Estadisticas de sesion");
+            }
 
-    break;
-    case 4:
+            break;
+        case 4:
+            {
+                Console.Clear ();
+                Console.WriteLine("Estadisticas reiniciadas");
+                regresaralmenu();
+            }
+        break;
+        case 5:
+            {
+                Console.WriteLine("Resumen final");
+            }
+        break;
+    }
+} while (opcion != 5);
 
-    break;
-    case 5:
 
-    break;
-    case 0:
 
-    break;
+void regresaralmenu()
+{
+    Console.WriteLine("Presione ENTER para regresar al menu");
+    Console.ReadKey();
 }
+
